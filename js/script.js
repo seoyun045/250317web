362f5620d8b96358b681e91e38957039b88dadf5
seoyun045 <soorim9808@naver.com>
2025-03-17 16:36:52 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const grid=$(".contents").isotope();
+
+$(".btns>li").click(function(){
+ $(this).addClass("active");
+ $(this).siblings().removeClass("active");
+
+ /*필터기능*/ 
+ let a=$(this).attr('data-filter')
+$(".contents").isotope({filter:a})
+
+
+
+
+})
+
+$(".fancybox").fancybox();
